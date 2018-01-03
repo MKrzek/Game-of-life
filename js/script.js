ff82c0d5682330763e88d92780c6b2b728f31843
Malgorzata <mkrzek@googlemail.com>
2018-01-03 12:45:48 +0000

    added board

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-
-
-
-
+function GameOfLife(height,width){
+    this.boardWidth=width;
+    this.boardHeight=height;
+    this.board=document.querySelector('#board');
     
-})
+    };
+    GameOfLife.prototype.createBoard=function(){
+        this.board.style.width = 10 * this.boardWidth + 'px';
+        this.board.style.height = 10 * this.boardHeight + 'px';
+        console.log(this.boardHeight);
+        console.log(this.boardWidth); 
+        var allCells = this.boardHeight * this.boardWidth;
+         
+         
+
+         for (var i = 0; i < allCells; i++) {
+            var newDiv = document.createElement('div');
+            this.board.appendChild(newDiv)
+            console.log (newDiv)
+         }
+    }
+var game = new GameOfLife (50, 50);
+game.createBoard();
+console.log (game.allCells)
+
+
+
+
+
+
+});
