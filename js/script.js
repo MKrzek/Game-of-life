916e39673860786edce08aa8d61943fb04a0b132
Malgorzata <mkrzek@googlemail.com>
2018-01-03 20:29:34 +0000

    added nextstate method

--- js/script.js
+++ js/script.js
@@ -42,16 +42,64 @@ function GameOfLife(height,width){
     };
 
     GameOfLife.prototype.firstGlider=function(x, y, state){
+        console.log (this);
         this.setCellState(x, y, state);
         this.setCellState(x-1, y-1, state);
         this.setCellState(x, y-1, state);
         this.setCellState(x+1, y+1, state);
         this.setCellState(x-1, y+1, state);
-    }
+    };
+    GameOfLife.prototype.computeCellNextState=function (x, y){
+        var lifeNeighbours=[];
+        var neighbour1=this.position(x-1, y-1);
+        var neighbour2=this.position(x, y-1);
+        var neighbour3=this.position(x+1, y-1);
+        var neighbour4=this.position(x-1, y);
+        var neighbour5=this.position(x+1, y);
+        var neighbour6=this.position(x-1, y+1);
+        var neighbour7=this.position(x, y+1);
+        var neighbour8=this.position(x+1, y+1);
+        var neighbours= [neighbour1, neighbour2, neighbour3, neighbour4, neighbour5, neighbour6, neighbour7, neighbour8];
+        neighbours.forEach(function(neighbour){
+            if (neighbour.classList.contains('live')){
+                lifeNeighbours.push(neighbour);
+                console.log (neighbour);
+            };
+            return lifeNeighbours;
+            
+        });
+        if (this.position(x, y).classList.contains('live') && lifeNeighbours.length<2){
+            console.log ('op1', 0);
+            return 0
+        };
+        if (this.position(x, y).classList.contains('live') && (lifeNeighbours.length===2||lifeNeighbours.length===3)){
+            console.log ('op2', 1)
+            return 1;
+        };
+        if (this.position(x, y).classList.contains('live') && lifeNeighbours.length>3){
+            console.log('op3', 0)
+            return 0;
+        };
+        if ((!this.position(x, y).classList.contains('live')) && lifeNeighbours.length===3){
+            console.log ('op4', 1)
+            return 1;
+        };
+
+    };
+
+
+
+
+
+
+
+
+
+
 var game = new GameOfLife (20, 20);
 game.createBoard();
-
 game.firstGlider(1, 1, 'live');
+game.computeCellNextState(1,1)
 
 
 
